6741d01925c874db25ad59ff9da9fd64875c28c8
Monica Davila <monica.davila510@gmail.com>
2026-04-30 14:03:49 -0500

    display DebounceSearchDemo in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import PaginationDemo from "./components/PaginationDemo";
+import DebounceSearchDemo from "./components/DebounceSearchDemo";
 import "./App.css";
 
 function App() {
   return (
     <main>
       <h1>Custom Hooks Lab</h1>
+
       <PaginationDemo />
+
+      <hr />
+
+      <DebounceSearchDemo />
     </main>
   );
 }
