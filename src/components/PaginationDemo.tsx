1d5cb154f84610e7ebca36e7e21c31714187386d
Monica Davila <monica.davila510@gmail.com>
2026-04-30 13:40:24 -0500

    display paginated items in PaginationDemo component

--- src/components/PaginationDemo.tsx
+++ src/components/PaginationDemo.tsx
@@ -1,3 +1,19 @@
+import usePagination from "../hooks/usePagination";
+
 export default function PaginationDemo() {
-  return <div>Pagination Demo</div>;
+  const items = Array.from({ length: 100 }, (_, index) => `Item ${index + 1}`);
+
+  const { startIndex, endIndex } = usePagination(items.length, 10);
+
+  const currentItems = items.slice(startIndex, endIndex + 1);
+
+  return (
+    <div>
+      <h2>Pagination Demo</h2>
+
+      {currentItems.map((item) => (
+        <p key={item}>{item}</p>
+      ))}
+    </div>
+  );
 }
